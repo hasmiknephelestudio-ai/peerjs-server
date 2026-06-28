3bb687bc9bacbcd1a2e72b170459650d28428a65
hasmiknephelestudio-ai <hasmik.nephelestudio@gmail.com>
2026-06-28 20:54:56 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const { PeerServer } = require('peer');
+
+const port = process.env.PORT || 9000;
+
+const peerServer = PeerServer({
+  port: port,
+  path: '/',
+  allow_discovery: true
+});
+
+console.log('PeerJS server running on port ' + port);
